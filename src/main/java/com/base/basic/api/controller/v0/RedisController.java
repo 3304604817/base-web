b8d806c9aadbf57e3f1099a4398e4e6f09200852
gaoyang <3304604817@qq.com>
2022-11-27 19:56:48 +0800

    MQ

---
+++ src/main/java/com/base/basic/api/controller/v0/RedisController.java
@@ -0,0 +1,16 @@
+package com.base.basic.api.controller.v0;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yang.gao
+ * @description
+ * @date 11/27/22 1:06 下午
+ */
+@Api(tags="redis")
+@RestController
+@RequestMapping("/rocketMQTemplate")
+public class RedisController {
+}
--- src/main/java/com/base/basic/app/service/impl/RedisServiceImpl.java
+++ src/main/java/com/base/basic/app/service/impl/RedisServiceImpl.java
@@ -1,6 +1,8 @@
 package com.base.basic.app.service.impl;
 
 import com.base.basic.app.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
@@ -9,4 +11,7 @@ import java.util.UUID;
 
 @Service
 public class RedisServiceImpl implements RedisService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 }
