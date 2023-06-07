4cf428addd69578b5dc138e6a70475ef27b62e28
yang.gao11 <yang.gao11@envision-aesc.com>
2023-06-07 19:00:48 +0800

    集成SOAP

--- src/main/java/com/base/basic/domain/exc/OldGoodsExcelModel.java
+++ src/main/java/com/base/basic/domain/exc/OldGoodsExcelModel.java
@@ -1,12 +1,8 @@
 package com.base.basic.domain.exc;
 
 import com.alibaba.excel.annotation.ExcelProperty;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-
 import java.math.BigDecimal;
 
-@Data
 public class OldGoodsExcelModel {
 
     @ExcelProperty(value = "商品唯一标志UUID" ,index = 0)
@@ -20,4 +16,36 @@ public class OldGoodsExcelModel {
 
     @ExcelProperty(value = "图片" ,index = 3)
     private String pictureUrl;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
 }
--- src/main/java/com/base/basic/domain/exc/UserExcelModel.java
+++ src/main/java/com/base/basic/domain/exc/UserExcelModel.java
@@ -1,11 +1,8 @@
 package com.base.basic.domain.exc;
 
 import com.alibaba.excel.annotation.ExcelProperty;
-import lombok.Data;
-
 import java.util.Date;
 
-@Data
 public class UserExcelModel {
 
     @ExcelProperty(value = "用户名" ,index = 0)
@@ -16,4 +13,28 @@ public class UserExcelModel {
 
     @ExcelProperty(value = "创建时间" ,index = 2)
     private Date creationDate;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 }
