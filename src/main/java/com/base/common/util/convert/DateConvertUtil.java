1a34bd7fdd5f54f1f4b5e1a5c32c9fefd9f1d7a3
yang.gao11 <yang.gao11@envision-aesc.com>
2023-06-12 09:19:06 +0800

    集成SOAP

--- src/main/java/com/base/common/util/convert/DateConvertUtil.java
+++ src/main/java/com/base/common/util/convert/DateConvertUtil.java
@@ -8,23 +8,27 @@ import java.util.List;
 
 public class DateConvertUtil {
 
-    /**
-     * 获取当前日期：yyyy-MM-dd HH:mm:ss
-     * @return
-     */
-    public static String nowTimeString(){
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return now.format(formatter);
-    }
+    public static String FORMAT1 = "yyyy-MM-dd HH:mm:ss";
+    public static String FORMAT2 = "yyyy-MM-dd HH:mm";
+    public static String FORMAT3 = "yyyy-MM-dd HH";
+    public static String FORMAT4 = "yyyy-MM-dd";
+    public static String FORMAT5 = "yyyy-MM";
+    public static String FORMAT6 = "yyyy";
+
+    public static String FORMAT7 = "yyyyMMddHHmmss";
+    public static String FORMAT8 = "yyyyMMddHHmm";
+    public static String FORMAT9 = "yyyyMMddHH";
+    public static String FORMAT10 = "yyyyMMdd";
+    public static String FORMAT11 = "yyyyMM";
+    public static String FORMAT12 = "yyyy";
 
     /**
-     * 获取当前时间：yyyy-MM-dd HH:mm:ss
+     * 获取当前日期：默认yyyy-MM-dd HH:mm:ss
      * @return
      */
-    public static String nowDataString(){
+    public static String nowDateString(String formatter){
         LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        return now.format(formatter);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatter);
+        return now.format(dateTimeFormatter);
     }
 }
