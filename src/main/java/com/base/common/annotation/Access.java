7327068558caf622ccaf51cee70b81f1a6b0a575
yang.gao11 <yang.gao11@envision-aesc.com>
2023-07-17 14:55:46 +0800

    用户管理

--- src/main/java/com/base/common/annotation/Access.java
+++ src/main/java/com/base/common/annotation/Access.java
@@ -5,5 +5,21 @@ import java.lang.annotation.*;
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Access {
+    /**
+     * 不登陆也能访问
+     * @return
+     */
     boolean accessNoToken() default false;
+
+    /**
+     * 只要登录就能访问
+     * @return
+     */
+    boolean accessToken() default false;
+
+    /**
+     * 需要权限才能访问
+     * @return
+     */
+    boolean accessPermissions() default true;
 }
