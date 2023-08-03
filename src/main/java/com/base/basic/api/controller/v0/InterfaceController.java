bbe883a848801acb9407dbe2d110d4d1be5d0e0b
yang.gao11 <yang.gao11@envision-aesc.com>
2023-08-03 11:30:41 +0800

    菜单管理

---
+++ src/main/java/com/base/basic/api/controller/v0/InterfaceController.java
@@ -0,0 +1,11 @@
+package com.base.basic.api.controller.v0;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags="接口管理")
+@RestController
+@RequestMapping("/interface")
+public class InterfaceController {
+}
--- src/main/java/com/base/basic/infra/mapper/InterfaceMapper.java
+++ src/main/java/com/base/basic/infra/mapper/InterfaceMapper.java
@@ -3,5 +3,9 @@ package com.base.basic.infra.mapper;
 import com.base.basic.domain.entity.v1.Interface;
 import com.base.common.util.mybatis.mapper.SupperMapper;
 
+import java.util.List;
+
 public interface InterfaceMapper extends SupperMapper<Interface> {
+
+    List<Interface> list(Interface param);
 }
