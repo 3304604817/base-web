98816a3e1c76f3361c012d615663232c5a3e0fe8
yang.gao11 <yang.gao11@envision-aesc.com>
2023-08-22 13:21:59 +0800

    javaFX错误引用

--- src/test/java/com/base/basic/DistinctTest.java
+++ src/test/java/com/base/basic/DistinctTest.java
@@ -1,13 +1,10 @@
 package com.base.basic;
 
-import javafx.scene.effect.Bloom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.*;
-import java.util.BitSet;
-import java.util.HashSet;
 import java.util.Random;
 
 /**
