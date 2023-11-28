7c458c6df95562eb5cd239e867486f089d510b7f
3304604817@qq.com <19960428gy>
2023-11-28 10:29:25 +0800

    数据库操作

--- src/main/java/com/base/basic/api/controller/v1/BatchScriptController.java
+++ src/main/java/com/base/basic/api/controller/v1/BatchScriptController.java
@@ -3,6 +3,7 @@ package com.base.basic.api.controller.v1;
 import com.base.basic.app.service.BatchScriptRedisService;
 import com.base.basic.domain.vo.v0.ScriptBodyVO;
 import com.base.basic.domain.vo.v0.ScriptParamVO;
+import com.base.basic.domain.vo.v0.TestConnectionVO;
 import com.base.common.annotation.Access;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +26,24 @@ public class BatchScriptController {
 
     @Autowired
     private BatchScriptRedisService batchScriptRedisService;
+
+    @ApiOperation(value = "测试redis连通性")
+    @PostMapping("/redis/connect")
+    public ResponseEntity<TestConnectionVO> redis(@RequestBody TestConnectionVO testConnection) {
+        Jedis jedis = new Jedis(testConnection.getAddress(), testConnection.getPort());
+        try {
+            if(StringUtils.isNotEmpty(testConnection.getPassword())){
+                jedis.auth(testConnection.getPassword());
+            }
+            String ping = jedis.ping();
+            testConnection.setConnected(StringUtils.equals(ping, "PONG") ? Boolean.TRUE : Boolean.FALSE);
+        }catch (Exception e){
+            testConnection.setConnected(Boolean.FALSE);
+        }finally {
+            jedis.close();
+        }
+        return new ResponseEntity(testConnection, HttpStatus.OK);
+    }
 
     @ApiOperation(value = "执行Redis脚本")
     @PostMapping("/execute/redis")
