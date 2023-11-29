451469578b551514fe6a66f8582235e38ca9899e
3304604817@qq.com <19960428gy>
2023-11-29 15:03:27 +0800

    数据库操作

--- src/main/java/com/base/basic/app/service/BatchScriptRedisService.java
+++ src/main/java/com/base/basic/app/service/BatchScriptRedisService.java
@@ -5,5 +5,8 @@ import com.base.basic.domain.vo.v0.ScriptParamVO;
 
 public interface BatchScriptRedisService {
 
+    /**
+     * 执行redis脚本
+     */
     void executeRedis(ScriptBodyVO scriptBody);
 }
