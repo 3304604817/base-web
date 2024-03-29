c2329fe9973cf4e851fa496214de003a7247e284
3304604817@qq.com <19960428gy>
2024-03-29 16:49:02 +0800

    集成Excel合并

--- src/main/java/com/base/basic/app/service/impl/ExcelServiceImpl.java
+++ src/main/java/com/base/basic/app/service/impl/ExcelServiceImpl.java
@@ -36,6 +36,7 @@ public class ExcelServiceImpl implements ExcelService {
 
                     if(null != cell1){
                         if(cell1.getCellType() == CellType.NUMERIC){
+                            // 数字不合并
 //                            cell1.setCellValue(cell1.getNumericCellValue() + "\n" + cell2.getNumericCellValue());
                         }else if(cell1.getCellType() == CellType.STRING && StringUtils.isNotEmpty(cell1.getStringCellValue())){
                             cell1.setCellValue(cell1.getStringCellValue() + "\n" + cell2.getStringCellValue());
