af6db3e56a7c087daa85fbb629ff71b1944bd6cc
Gshen <gshen1024@icloud.com>
2025-03-16 11:05:03 +0800

    启动脚本调整

--- src/test/java/com/base/basic/BasicTest.java
+++ src/test/java/com/base/basic/BasicTest.java
@@ -19,30 +19,6 @@ public class BasicTest {
     public void test() throws UnknownHostException, SocketException {
         System.out.println("--------------------------------------------------");
 
-        for(String aaa:LocalAddressUtils.getIpv6()){
-            System.out.println(aaa);
-        }
-
-        // 获取所有的网络接口
-//        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-//
-//        while (networkInterfaces.hasMoreElements()) {
-//            NetworkInterface networkInterface = networkInterfaces.nextElement();
-//
-//            // 获取每个网络接口的IP地址
-//            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-//
-//            while (inetAddresses.hasMoreElements()) {
-//                InetAddress inetAddress = inetAddresses.nextElement();
-//
-//                // 检查是否是IPv6地址
-//                if (inetAddress instanceof Inet6Address) {
-//                    System.out.println("Interface: " + networkInterface.getName());
-//                    System.out.println("IPv6 Address: " + inetAddress.getHostAddress());
-//                }
-//            }
-//        }
-
 
         System.out.println("--------------------------------------------------");
     }
