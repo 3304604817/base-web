54d0c3bad7a8f904459684397f5d629abec44db8
Gshen <gshen1024@icloud.com>
2025-03-16 17:27:40 +0800

    本机信息推送

--- src/main/java/com/base/basic/job/LocalMsgJobHandler.java
+++ src/main/java/com/base/basic/job/LocalMsgJobHandler.java
@@ -3,7 +3,9 @@ package com.base.basic.job;
 
 import com.base.common.scheduled.runner.ScheduledRunner;
 import com.base.common.util.local.LocalAddressUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -17,20 +19,25 @@ import java.util.Map;
 @Component("LocalMsgJobHandler")
 public class LocalMsgJobHandler implements ScheduledRunner {
 
+    @Value("${spring.mail.username}")
+    private String from;
+
     @Autowired
     JavaMailSender javaMailSender;
 
     @Override
     public void run(String args) {
         try {
+            JSONObject jsonObject = new JSONObject(args);
+
             // 构建一个邮件对象
             SimpleMailMessage message = new SimpleMailMessage();
             // 设置邮件主题
             message.setSubject("本机信息");
             // 设置邮件发送者，这个跟application.yml中设置的要一致
-            message.setFrom("3304604817@qq.com");
+            message.setFrom(from);
             // 设置邮件接收者，可以有多个接收者，中间用逗号隔开，以下类似
-            message.setTo("3304604817@qq.com");
+            message.setTo(jsonObject.getString("to"));
 
             // 设置邮件的正文
             StringBuffer text = new StringBuffer();
